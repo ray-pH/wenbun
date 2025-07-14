4e58f59ffb5c7b948b9252b25137c96cc31a9740
ray-pH <rayhan.azizi9@gmail.com>
2025-07-14 22:52:17 +0700

    remove unused files

--- src/stores/stores.ts
+++
@@ -1,7 +0,0 @@
-import { writable, type Writable } from 'svelte/store';
-import type { Profile } from '../lib/profile';
-
-export const profileStore: Writable<Profile> = writable({
-    decks: [],
-    deckData: {},
-});
