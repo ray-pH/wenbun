5989c36a927d25114139667b55e13dfbfa1550ab
ray-pH <rayhan.azizi9@gmail.com>
2025-07-30 06:48:54 +0700

    [wenbun] remove console log when service worker is successfully registered

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,9 @@
         if ('serviceWorker' in navigator) {
             navigator.serviceWorker
                 .register(`${base}/service-worker.js`)
-                .then(reg => console.log('SW registered:', reg))
+                .then(reg => {
+                    // console.log('SW registered:', reg)
+                })
                 .catch(err => console.error('SW registration failed:', err));
         }
     })
