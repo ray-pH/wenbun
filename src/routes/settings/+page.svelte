462b7343774d0262e2efd6bfbc8a2759d203b25b
ray-pH <rayhan.azizi9@gmail.com>
2025-09-28 16:30:24 +0700

    [PWA] don't show install PWA button when already on PWA

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -6,7 +6,7 @@
     import _ from "lodash";
     import SettingsItem from "./SettingsItem.svelte";
     import * as FSRS from "ts-fsrs"
-    import { type DeepRequired } from "$lib/util";
+    import { isRunningInPWA, type DeepRequired } from "$lib/util";
     import { ChineseToneColorPalette } from "$lib/constants";
     import { ChineseMandarinReading } from '$lib/chinese';
     import ProfileLogin from './ProfileLogin.svelte';
@@ -359,13 +359,15 @@
         <div class="settings-section">
             <div class="section-title">Offline Data</div>
             <div class="section-container">
-                <div class="note">
-                    This app can be installed as PWA and can be accessed offline.
-                </div>
-                <button class="button" onclick={() => showPWAInstallDialog()}>
-                    <i class="fa-solid fa-download"></i>&nbsp;
-                    Install as PWA
-                </button>
+                {#if !isRunningInPWA()}
+                    <div class="note">
+                        This app can be installed as PWA and can be accessed offline.
+                    </div>
+                    <button class="button" onclick={() => showPWAInstallDialog()}>
+                        <i class="fa-solid fa-download"></i>&nbsp;
+                        Install as PWA
+                    </button>
+                {/if}
                 <a class="button" href={`${base}/offline-data`} aria-label="Offline Data">
                     <i class="fa-solid fa-gear"></i>&nbsp;
                     Go to Offline Data Settings
