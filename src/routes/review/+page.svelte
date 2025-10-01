d218e84246642408a4daa21104bb3ff53acc3e31
ray-pH <rayhan.azizi9@gmail.com>
2025-10-01 23:25:04 +0700

    [review] reduce the probability of getting the same card in a row

--- src/routes/review/+page.svelte
+++ src/routes/review/+page.svelte
@@ -99,12 +99,22 @@
             characterWriterRef.stopAllAudio();
         }
     }
+    
+    const MAX_NEXT_CARD_RETRY_COUNT = 10;
+    function getNextCardId(): number | undefined {
+        //TODO: avoid duplication in a deterministic way
+        let id = undefined;
+        for (let i = 0; i < MAX_NEXT_CARD_RETRY_COUNT; i++) {
+            id = app.getNextCard(deckId, data.reviewMode);
+            if (id === undefined || id !== currentCardId) return id;
+        }
+    }
 
     function nextCard() {
         if (forceStopAudioOnNextCard) stopAudio();
         resetState();
         isCardChanged = true;
-        const id = app.getNextCard(deckId, data.reviewMode);
+        const id = getNextCardId();
         if (id === undefined) {
             // done for today
             isDoneToday = true;
