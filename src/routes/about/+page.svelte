ee2615c241d29e8aecc5f79880d1144a0e5ecf7a
ray-pH <rayhan.azizi9@gmail.com>
2026-02-16 23:11:02 +0700

    update changelog

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -22,6 +22,14 @@
     </div>
     <div class="changelog-container">
         Changelog
+        
+        <h4>v0.23.0 (2026-02-16)</h4>
+        <ul>
+            <li>
+                <b>Review</b>
+                Added the ability to setup a keyboard key to advance to the next card.
+            </li>
+        </ul>
 
         <h4>v0.22.0 (2026-01-25)</h4>
         <ul>
